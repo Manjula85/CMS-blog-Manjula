8ad5cb098e120a81cc8edb666aaf6947cefb3137
Manjula Guneratne <uftcodingbootcamp@gmail.com>
2021-03-16 22:30:35 -0400

    Post model created

--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 //index file is the first place that is accessed in the models folder.
 
 const User = require('./User');
+const Post = require('./Post');
 
-module.exports = { User };
+module.exports = { User, Post };
