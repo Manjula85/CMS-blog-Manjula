ae749cc7fd25603dce2c1d1affa3491aec372fd2
Manjula Guneratne <uftcodingbootcamp@gmail.com>
2021-05-04 18:10:25 -0400

    getting the link to work still

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -3,33 +3,57 @@ const sequelize = require("../config/connection");
 const { Post, User, Comment } = require("../models");
 const withAuth = require("../utils/auth"); // <--- Not used still (add it to proper, POST, PUT and DELETE Methods)
 
-router.get('/', (req,res) => {
-    Post.findAll({
-        // where: {
-        //     //using the ID from the session
-        //     user_id: req.session.user_id
-        // },
-        attributes: [
-            'id',
-            'title',
-            'content',
-            'created_at'
-        ]
+router.get("/", (req, res) => {
+  Post.findAll({
+    attributes: ["id", "title", "content", "created_at"],
+  })
+    .then((dbPostData) => {
+      // serialize data before passing to template
+      const posts = dbPostData.map((post) => post.get({ plain: true }));
+      res.render("dashboard", { posts, loggedIn: true });
     })
-        .then(dbPostData => {
-            // serialize data before passing to template
-            const posts = dbPostData.map(post => post.get({plain: true}));
-            res.render('dashboard', { posts, loggedIn: true });
-        })
-        .catch(err => {
-            console.log(err);
-            res.status(500).json(err);
-        });
-
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
-// router.get("/", (req, res) => {
-//   res.render("dashboard", { loggedIn: true });
-// });
+//get by id number
+router.get("/:id", (req, res) => {
+  Post.findOne({
+    where: {
+      id: req.params.id,
+    },
+    attributes: ["id", "title", "content", "created_at"],
+    include: [
+      {
+        model: User,
+        attributes: ["username"],
+      },
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
+    ],
+  })
+    .then((dbPostData) => {
+      if (!dbPostData) {
+        res.status(404).json({ message: "No post found with this id" });
+        return;
+      }
+
+      // serialize data before passing to template
+      const posts = dbPostData.map((post) => post.get({ plain: true }));
+      res.render("dash-edit-del", { posts, loggedIn: true });
+      console.log("************ did it get here???");
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
+});
 
 module.exports = router;
